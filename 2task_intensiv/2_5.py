bb39ad1f39ec12b67c361b823da96e6123c2ddeb
Shetani <shetani@Air-Shetani.info-lan.me>
2026-06-13 14:53:02 +0300

    ebaniy fail v file 2_5

--- 2task_intensiv/2_5.py
+++ 2task_intensiv/2_5.py
@@ -6,8 +6,3 @@ for x in range(2):
                 f = w and ((x <= y) == (y <= z))
                 if f == True:
                     print(x,y,z,w,f)
-
-x y z w f
-0 0 0 1 True
-0 0 1 1 True
-0 1 1 1 True
